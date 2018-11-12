1720258cca92d68764f792c100d48919d627b7c3
jisan222 <44962537+jisan222@users.noreply.github.com>
2018-11-12 16:40:57 +0900

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//THE LARGE IMAGE
+let largeImg = document.getElementById('current');
+let img1=document.getElementById('img1');
+let img2=document.getElementById('img2');
+let img3=document.getElementById('img3');
+let img4=document.getElementById('img4');
+let img5=document.getElementById('img5');
+let img6=document.getElementById('img6');
+let img7=document.getElementById('img7');
+let img8=document.getElementById('img8');
+
+img1.addEventListener('click', function(event){
+  largeImg.src = event.target.src;
+});
+img2.addEventListener('click', function(event){
+  largeImg.src = event.target.src;
+});
+img3.addEventListener('click', function(event){
+  largeImg.src = event.target.src;
+});
+img4.addEventListener('click', function(event){
+  largeImg.src = event.target.src;
+});
+img5.addEventListener('click', function(event){
+  largeImg.src = event.target.src;
+});
+img6.addEventListener('click', function(event){
+  largeImg.src = event.target.src;
+});
